446104f1bb477edc795583b3f7f29dfbea9eaa9d
Davi Figueiredo <davi.mello.28@gmail.com>
2023-05-25 01:46:45 -0300

    feat: add new structs, implement ownable and counters, implement other checks and tests

--- __tests__/OnChainForms.spec.ts
+++ __tests__/OnChainForms.spec.ts
@@ -1,133 +1,330 @@
-import { Contract, Wallet} from "ethers";
-import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-import { deployContract } from "ethereum-waffle";
-import { expect } from "chai";
-import { ethers } from "hardhat";
+import { Contract, Transaction, Wallet } from 'ethers'
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { deployContract } from 'ethereum-waffle' // this adds expect(onChainForms.address).to.be.properAddress (???)
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
 
-describe("OnChainForms", () => {
+describe('OnChainForms', () => {
+  let onChainForms: Contract
+  let owner: SignerWithAddress
+  let others: SignerWithAddress[]
 
-  let onChainForms: Contract;
-  let owner: SignerWithAddress;
-  let others: SignerWithAddress[];
+  async function getFromEvents(
+    firstPromise: any,
+    eventName: string
+  ): Promise<any> {
+    const tx = firstPromise
+    const receipt = await tx.wait()
+    const event = receipt.events?.find((e: any) => e.event === eventName)
+    return event?.args
+  }
 
-beforeEach(async () => {
-    const signers = await ethers.getSigners();
-    owner = signers[0];
-    others = signers.slice(1);
+  beforeEach(async () => {
+    const OnChainForms = await ethers.getContractFactory('OnChainForms')
+    onChainForms = await OnChainForms.deploy()
+    await onChainForms.deployed()
+    ;[owner, ...others] = await ethers.getSigners()
+  })
 
-    const OnChainForms = await ethers.getContractFactory("OnChainForms");
-    onChainForms = await OnChainForms.deploy();
-    await onChainForms.deployed();
-});
+  it('Should deploy successfully', async () => {
+    expect(onChainForms.address).to.be.properAddress
+  })
 
-  it("Should deploy successfully", async () => {
-    expect(onChainForms.address).to.be.properAddress;
-  });
+  it('Should create a form successfully', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
 
-  it("Should create a form successfully", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
-    const form = await onChainForms.getForm(1);
-    expect(form.title).to.equal(title);
-    expect(form.questions).to.deep.equal(questions);
-  });
+    for (let question of questions) {
+      await onChainForms.createForm(title)
+      await onChainForms.addQuestionToForm(
+        1,
+        question.title,
+        question.description
+      )
+    }
 
-  it("Should add responders successfully", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
+    const formDetails = await onChainForms.getFormDetails(1)
+    expect(formDetails.title).to.equal(title)
+    expect(formDetails.questionsCount).to.equal(questions.length)
 
-    const responders = others.slice(0, 2).map((wallet) => wallet.address);
-    await onChainForms.addResponders(1, responders);
+    for (let i = 0; i < questions.length; i++) {
+      let questionDetails = await onChainForms.getQuestionDetails(1, i)
+      expect(questionDetails.title).to.equal(questions[i].title)
+      expect(questionDetails.description).to.equal(questions[i].description)
+    }
+  })
+
+  it('Should add responders successfully', async () => {
+    const title = 'Test Form2'
+    const questions = [
+      { title: 'Question 1', description: 'Desc 1' },
+      { title: 'Question 2', description: 'Desc 2' }
+    ]
+
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
+
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
+
+    const responders = [others[0].address]
+    await onChainForms.addResponders(formId, responders)
 
     for (const responder of responders) {
-      const isAllowed = await onChainForms.allowedResponders(1, responder);
-      expect(isAllowed).to.equal(true);
+      const isAllowed = await onChainForms.isAllowedResponder(formId, responder)
+      expect(isAllowed).to.equal(true)
     }
-  });
+  })
 
-  it("Should submit a response successfully", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
+  it('Should submit a response successfully', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' }
+    ]
 
-    const responders = others.slice(0, 2).map((wallet) => wallet.address);
-    await onChainForms.addResponders(1, responders);
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
 
-    const [responder] = responders;
-    const formIndex = 1;
-    const questionIndex = 0;
-    const response = "Answer 1";
+    let questionIndex
+    for (let question of questions) {
+      const events = await getFromEvents(
+        await onChainForms.addQuestionToForm(
+          formId,
+          question.title,
+          question.description
+        ),
+        'QuestionCreated'
+      )
+      questionIndex = events.questionIndex
+    }
 
-    await onChainForms.connect(others[0]).submitResponse(formIndex, questionIndex, response);
+    await onChainForms.addResponder(formId, others[0].address)
 
-    const responseHistory = await onChainForms.getResponseHistory(formIndex, responder, questionIndex);
-    expect(responseHistory.responseHistory[0]).to.equal(response);
-  });
+    const response = 'Answer 1'
 
-  it("Should not allow unauthorized responders to submit responses", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response)
+    const responseHistory = await onChainForms.getResponseHistory(
+      formId,
+      questionIndex
+    )
+    expect(responseHistory.responses[0]).to.equal(response)
+  })
 
-    const formIndex = 1;
-    const questionIndex = 0;
-    const response = "Answer 1";
+  it('Should not allow unauthorized responders to submit responses', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
 
-    await expect(onChainForms.connect(others[0]).submitResponse(formIndex, questionIndex, response)).to.be.revertedWith("Not an allowed responder");
-  });
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
 
-  it("Should not allow submitting responses with invalid question index", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
 
-    const responders = others.slice(0, 2).map((wallet) => wallet.address);
-    await onChainForms.addResponders(1, responders);
+    const responder = others[0].address
+    const questionIndex = 0
+    const response = 'Answer 1'
 
-    const formIndex = 1;
-    const questionIndex = 2;
-    const response = "Answer 1";
+    await expect(
+      onChainForms
+        .connect(others[0])
+        .submitResponse(formId, questionIndex, response)
+    ).to.be.revertedWith('Not an allowed responder')
+  })
 
-    await expect(onChainForms.connect(others[0]).submitResponse(formIndex, questionIndex, response)).to.be.revertedWith("Invalid question index")
-  });
+  it('Should not allow submitting responses with invalid question index', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
 
-  it("Should not allow non-owners to create forms", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"]
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
 
-    await expect(onChainForms.connect(others[0]).createForm(title, questions)).to.be.revertedWith("Caller is not the owner");
-  });
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
 
-  it("Should not allow non-owners to add responders", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
+    const responders = others.slice(0, 2).map(wallet => wallet.address)
+    await onChainForms.addResponders(formId, responders)
 
-    const responders = others.slice(0, 2).map((wallet) => wallet.address);
+    const questionIndex = 2 // Invalid question index
+    const response = 'Answer 1'
 
-    await expect(onChainForms.connect(others[0]).addResponders(1, responders)).to.be.revertedWith("Caller is not the owner");
-  });
+    await expect(
+      onChainForms
+        .connect(others[0])
+        .submitResponse(formId, questionIndex, response)
+    ).to.be.revertedWith('Invalid question index')
+  })
 
-  it("Should get the correct response history and timestamps", async () => {
-    const title = "Test Form";
-    const questions = ["Question 1", "Question 2"];
-    await onChainForms.createForm(title, questions);
-    const responders = others.slice(0, 2).map((wallet) => wallet.address);
-    await onChainForms.addResponders(1, responders);
+  it('Should not allow non-owners to create forms', async () => {
+    const title = 'Test Form'
+    await expect(
+      onChainForms.connect(others[0]).createForm(title)
+    ).to.be.revertedWith('Ownable: caller is not the owner')
+  })
 
-    const [responder] = responders;
-    const formIndex = 1;
-    const questionIndex = 0;
-    const response1 = "Answer 1";
-    const response2 = "Answer 2";
+  it('Should not allow non-owners to add responders', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
 
-    await onChainForms.connect(others[0]).submitResponse(formIndex, questionIndex, response1);
-    await onChainForms.connect(others[0]).submitResponse(formIndex, questionIndex, response2);
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
 
-    const responseHistory = await onChainForms.getResponseHistory(formIndex, responder, questionIndex);
-    expect(responseHistory.responseHistory).to.deep.equal([response1, response2]);
-    expect(responseHistory.timestamps[0]).to.lessThan(responseHistory.timestamps[1]);
-  });
-});
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
+
+    const responders = others.slice(0, 2).map(wallet => wallet.address)
+
+    await expect(
+      onChainForms.connect(others[0]).addResponders(formId, responders)
+    ).to.be.revertedWith('Ownable: caller is not the owner')
+  })
+
+  it('Should get the correct response history and timestamps', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
+
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
+
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
+
+    const responders = others.slice(0, 2).map(wallet => wallet.address)
+    await onChainForms.addResponders(formId, responders)
+
+    const [responder] = responders
+    const questionIndex = 0
+    const response1 = 'Answer 1'
+    const response2 = 'Answer 2'
+
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response1)
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response2)
+
+    const responseHistory = await onChainForms.getResponseHistory(
+      formId,
+      questionIndex
+    )
+    expect(responseHistory.responses).to.deep.equal([response1, response2])
+    expect(responseHistory.timestamps[0]).to.lessThan(
+      responseHistory.timestamps[1]
+    )
+  })
+
+  it('Should retrieve all responses from a particular responder', async () => {
+    const title = 'Test Form'
+    const questions = [
+      { title: 'Question 1', description: 'Describe question 1' },
+      { title: 'Question 2', description: 'Describe question 2' }
+    ]
+
+    const { formId } = await getFromEvents(
+      await onChainForms.createForm(title),
+      'FormCreated'
+    )
+
+    for (let question of questions) {
+      await onChainForms.addQuestionToForm(
+        formId,
+        question.title,
+        question.description
+      )
+    }
+
+    const responders = others.slice(0, 2).map(wallet => wallet.address)
+    await onChainForms.addResponders(formId, responders)
+
+    const [responder] = responders
+    const questionIndex = 0
+    const response1 = 'Answer 1'
+    const response2 = 'Answer 2'
+
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response1)
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response2)
+
+    let responseHistory = await onChainForms.getResponseHistory(
+      formId,
+      questionIndex
+    )
+
+    expect(responseHistory.responses).to.deep.equal([response1, response2])
+
+    const response3 = 'Answer 3'
+
+    await onChainForms
+      .connect(others[0])
+      .submitResponse(formId, questionIndex, response3)
+
+    responseHistory = await onChainForms.getResponseHistory(
+      formId,
+      questionIndex
+    )
+
+    expect(responseHistory.responses).to.deep.equal([
+      response1,
+      response2,
+      response3
+    ])
+  })
+})
+
--- contracts/OnChainForms.sol
+++ contracts/OnChainForms.sol
@@ -1,74 +1,156 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract OnChainForms {
-    uint256 public formCount;
-    address public owner;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract OnChainForms is Ownable {
+    using Counters for Counters.Counter;
+    
+    event FormCreated(uint formId, string title);
+    event QuestionCreated(uint questionIndex, string title);
+
+    Counters.Counter private _formIds;
+    mapping(uint => Form) public forms;
+
+    struct Response {
+        string response;
+        uint timestamp;
+    }
+
+    struct Question {
+        string title;
+        string description;
+        Counters.Counter responsesCount;
+        mapping(uint => Response) responses;
+    }
 
     struct Form {
         string title;
-        string[] questions;
-        uint numResponses;
+        Counters.Counter questionsCount;
+        mapping(uint => Question) questions;
+        Counters.Counter responsesCount;
+        mapping(address => bool) allowedResponders;
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-
-    mapping(uint => Form) public forms;
-
-    mapping(uint => mapping(address => mapping(uint => string[]))) public responsesByTimestamp;
-    mapping(uint => mapping(address => uint[])) public responseTimestamps;
-    mapping(uint => mapping(uint => address)) public responders;
-    mapping(uint => mapping(address => bool)) public responseExists;
-    mapping(uint => mapping(address => bool)) public allowedResponders;
-
-    modifier onlyOwner {
-        require(msg.sender == owner, "Caller is not the owner");
-        _;
-    }
-
-    function addResponders(uint _formIndex, address[] memory _responders) public onlyOwner {
+    function addResponders(uint _formId, address[] memory _responders) public onlyOwner {
+        Form storage form = forms[_formId];
         for (uint i = 0; i < _responders.length; i++) {
-            allowedResponders[_formIndex][_responders[i]] = true;
+            form.allowedResponders[_responders[i]] = true;
         }
     }
 
-    function createForm(string memory _title, string[] memory _questions) public onlyOwner  {
-        formCount++;
-        uint formIndex = formCount;
-        forms[formIndex] = Form({
-            title: _title,
-            questions: _questions,
-            numResponses: 0
-        });
+    function addResponder(uint _formId, address _responder) public onlyOwner {
+        Form storage form = forms[_formId];
+        form.allowedResponders[_responder] = true;
     }
 
-    function submitResponse(uint _formIndex, uint _questionIndex, string memory _response) public {
-        require(allowedResponders[_formIndex][msg.sender], "Not an allowed responder");
-        Form storage form = forms[_formIndex];
-        require(_questionIndex < form.questions.length, "Invalid question index");
+    function isAllowedResponder(uint _formId, address _responder) public view returns (bool) {
+        Form storage form = forms[_formId];
+        return form.allowedResponders[_responder];
+    }
+
+    function createForm(string memory _title) public onlyOwner returns (uint formId) {
+        _formIds.increment();
+        formId = _formIds.current();
+        forms[formId].title = _title;
+        emit FormCreated(formId, _title);
+    }
+
+    function addQuestionToForm(uint _formId, string memory _questionTitle, string memory _questionDescription) public onlyOwner {
+        Form storage form = forms[_formId];
+        uint questionIndex = form.questionsCount.current();
+        form.questions[questionIndex].title = _questionTitle;
+        form.questions[questionIndex].description = _questionDescription;
+        emit QuestionCreated(questionIndex, _questionTitle);
+        form.questionsCount.increment();
+    }
+
+    function submitResponse(uint _formId, uint _questionIndex, string memory _response) public {
+        Form storage form = forms[_formId];
+        require(form.allowedResponders[msg.sender], "Not an allowed responder");
+        require(_questionIndex < form.questionsCount.current(), "Invalid question index");
+
+        Question storage question = form.questions[_questionIndex];
 
         uint timestamp = block.timestamp;
-        responsesByTimestamp[_formIndex][msg.sender][_questionIndex].push(_response);
-        responseTimestamps[_formIndex][msg.sender].push(timestamp);
+        Response memory newResponse = Response(_response, timestamp);
+        uint responseIndex = question.responsesCount.current();
+        question.responses[responseIndex] = newResponse;
+        question.responsesCount.increment();
 
-        if (!responseExists[_formIndex][msg.sender]) {
-            responders[_formIndex][form.numResponses] = msg.sender;
-            responseExists[_formIndex][msg.sender] = true;
-            form.numResponses++;
+        if (question.responsesCount.current() == 1) {
+            form.responsesCount.increment();
         }
     }
 
-    function getForm(uint _formIndex) public view returns (string memory title, string[] memory questions) {
-        Form storage form = forms[_formIndex];
-        return (form.title, form.questions);
+    function getForm(uint _formId) public view returns (
+        string memory title, 
+        uint questionsCount, 
+        uint responsesCount, 
+        string[] memory questionTitles, 
+        string[] memory questionDescriptions
+    ) {
+        Form storage form = forms[_formId];
+        questionTitles = new string[](form.questionsCount.current());
+        questionDescriptions = new string[](form.questionsCount.current());
+
+        for (uint i = 0; i < form.questionsCount.current(); i++) {
+            questionTitles[i] = form.questions[i].title;
+            questionDescriptions[i] = form.questions[i].description;
+        }
+
+        return (
+            form.title, 
+            form.questionsCount.current(), 
+            form.responsesCount.current(), 
+            questionTitles, 
+            questionDescriptions
+        );
     }
 
-    function getResponseHistory(uint _formIndex, address _responder, uint _questionIndex) public view returns (string[] memory responseHistory, uint[] memory timestamps) {
-        responseHistory = responsesByTimestamp[_formIndex][_responder][_questionIndex];
-        timestamps = responseTimestamps[_formIndex][_responder];
-        return (responseHistory, timestamps);
+    function getFormDetails(uint _formId) public view returns (
+        string memory title, 
+        uint questionsCount, 
+        uint responsesCount
+    ) {
+        Form storage form = forms[_formId];
+        return (
+            form.title, 
+            form.questionsCount.current(), 
+            form.responsesCount.current()
+        );
+    }
+
+    function getQuestionDetails(uint _formId, uint _questionIndex) public view returns (
+        string memory title,
+        string memory description
+    ) {
+        Form storage form = forms[_formId];
+        require(_questionIndex < form.questionsCount.current(), "Invalid question index");
+
+        Question storage question = form.questions[_questionIndex];
+
+        return (
+            question.title, 
+            question.description
+        );
+    }
+
+    function getResponseHistory(uint _formId, uint _questionIndex) public view
+        returns (string[] memory responses, uint[] memory timestamps)
+    {
+        Form storage form = forms[_formId];
+        require(_questionIndex < form.questionsCount.current(), "Invalid question index");
+
+        Question storage question = form.questions[_questionIndex];
+        responses = new string[](question.responsesCount.current());
+        timestamps = new uint[](question.responsesCount.current());
+
+        for (uint i = 0; i < question.responsesCount.current(); i++) {
+            responses[i] = question.responses[i].response;
+            timestamps[i] = question.responses[i].timestamp;
+        }
+
+        return (responses, timestamps);
     }
 }
