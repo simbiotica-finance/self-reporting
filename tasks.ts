85f7075b7e9603b04fdd9d7391b326ae941103ca
Davi Figueiredo <davi.mello.28@gmail.com>
2023-05-26 17:24:35 -0300

    feat: add task to process json

--- tasks.ts
+++ tasks.ts
@@ -1,70 +1,126 @@
-
-const fs = require("node:fs");
-const { exec } = require("child_process");
+const fs = require("fs");
 const { task, types } = require("hardhat/config");
 const getFromEvents = require("./utils/getFromEvents");
 
 interface ActionParams {
   title: string;
-  form?: number;
+  formId?: number;
+  required?: boolean;
+  responseType?: number;
   contract: string;
   description: string;
+  skipDependencyCheck?: boolean;
 }
 
 interface ActionReturn {
-  title: string;
-  contract: string;
+  [key: string]: any;
+}
+
+interface TaskConfig {
+  name: string;
+  contract?: string;
+  formId?: number;
+  title?: string;
+  description?: string;
+  responseType?: number;
+  required?: number;
 }
 
 task("deploy", "Deploy the contract")
-   .setAction( async ( _: any, hre: any ) => {
-      const accounts = await hre.web3.eth.getAccounts()
+  .addFlag("skipDependencyCheck", "Skip dependency check")
+  .setAction(async ({ skipDependencyCheck }, hre: any): Promise<ActionReturn> => {
+    if (!skipDependencyCheck && !hre.dependencies.hasOwnProperty("contract")) {
+      throw new Error("Missing dependency: contract");
+    }
 
-      const OnChainForms = await hre.ethers.getContractFactory("OnChainForms", accounts[0]);
+    const accounts = await hre.web3.eth.getAccounts();
 
-      const deploy = await OnChainForms.deploy();
+    const OnChainForms = await hre.ethers.getContractFactory("OnChainForms", accounts[0]);
 
-      await deploy.deployed();
+    const deploy = await OnChainForms.deploy();
 
-      console.log("OnChainForms deployed to:", deploy.address);
-  })
+    await deploy.deployed();
+
+    console.log("OnChainForms deployed to:", deploy.address);
+
+    return {
+      contract: deploy.address,
+    };
+  });
 
 task("add-form", "Create a form")
-   .addParam("contract", "The form address.")
-   .addParam("title", "Create a title for the form", "Default Title")
-   .addParam("description", "Create a description for the form", "Default Description")
-   .setAction(async ({ title, contract, description}: ActionParams, hre: any): Promise<void> => {
+  .addFlag("skipDependencyCheck", "Skip dependency check")
+  .addParam("contract", "The form address.")
+  .addParam("title", "Create a title for the form", "Default Title")
+  .addParam("description", "Create a description for the form", "Default Description")
+  .setAction(async ({ contract, title, description, skipDependencyCheck }: ActionParams, hre: any): Promise<ActionReturn> => {
+    if (!skipDependencyCheck && !hre.dependencies.hasOwnProperty("contract")) {
+      throw new Error("Missing dependency: contract");
+    }
 
-      const OnChainForms = await hre.ethers.getContractAt("OnChainForms", contract);
+    const OnChainForms = await hre.ethers.getContractAt("OnChainForms", contract);
 
-      const tx = await OnChainForms.createForm(title, description, {
-         gasLimit: 100000,
-         gasPrice: 100000000000,
-      });
+    const tx = await OnChainForms.createForm(title, description, {
+      gasLimit: 1000000,
+      gasPrice: 100000000000,
+    });
 
-      const { formId } = await getFromEvents(tx, "FormCreated");
+    const { formId } = await getFromEvents(tx, "FormCreated");
 
-      console.log("New form:", formId);
-   });
+    console.log("New form:", formId);
 
+    return {
+      formId: formId.toNumber(),
+    };
+  });
 
 task("add-question", "Create a question on a form")
-   .addParam("contract", "The form address.", undefined, types.string)
-   .addParam("form", "The form id.", undefined, types.int)
-   .addParam("title", "Create a title for the form", "Default Question Title", types.string)
-   .addParam("responseType", "What type of response is required?", "String", types.string)
-   .addParam("required", "Is the question required?", false, types.boolean)
-   .addParam("description", "Create a description for the form", "Default Question Description", types.string)
-   .setAction(async ({ form, title, contract, description}: ActionParams, hre: any): Promise<void> => {
+  .addFlag("skipDependencyCheck", "Skip dependency check")
+  .addParam("contract", "The form address.", undefined, types.string)
+  .addParam("formId", "The form id.", undefined, types.int)
+  .addParam("title", "Create a title for the form", "Default Question Title")
+  .addParam("responseType", "What type of response is required?", undefined, types.int)
+  .addParam("required", "Is the question required?", false, types.boolean)
+  .addParam("description", "Create a description for the form", "Default Question Description", types.string)
+  .setAction(async ({ formId, title, contract, description, required, responseType, skipDependencyCheck }: ActionParams, hre: any): Promise<ActionReturn> => {
+    if (!skipDependencyCheck && !hre.dependencies.hasOwnProperty("contract")) {
+      throw new Error("Missing dependency: contract");
+    }
 
-      const OnChainForms = await hre.ethers.getContractAt("OnChainForms", contract);
+    const OnChainForms = await hre.ethers.getContractAt("OnChainForms", contract);
 
-      const tx = await OnChainForms.addQuestionToForm(form, title, description, {
-         gasLimit: 100000,
-         gasPrice: 100000000000,
-      });
+    const tx = await OnChainForms.addQuestionToForm(formId, title, description, required, responseType, {
+      gasLimit: 1000000,
+      gasPrice: 100000000000,
+    });
 
-      const {questionIndex} = await getFromEvents(tx, "QuestionCreated");
+    const { questionIndex } = await getFromEvents(tx, "QuestionCreated");
 
-      console.log("New question:", questionIndex);
-   });
+    console.log("New question:", questionIndex);
+
+    return {
+      questionIndex: questionIndex.toNumber(),
+    };
+  });
+
+task("process-json", "Process tasks from JSON file")
+  .addParam("json", "Path to the JSON file")
+  .setAction(async ({ json }, hre) => {
+    const jsonContent = fs.readFileSync(json, "utf8");
+    const tasks: TaskConfig[] = JSON.parse(jsonContent);
+
+    const results: ActionReturn = {};
+
+    for (const taskConfig of tasks) {
+      const { name, ...params } = taskConfig;
+
+      try {
+        const result = await hre.run(name, { ...params, skipDependencyCheck: true, ...results });
+        Object.assign(results, result);
+      } catch (error) {
+        console.error(`Error executing task '${name}': ${error.message}`);
+      }
+    }
+
+    console.log("Results:", results);
+  });
